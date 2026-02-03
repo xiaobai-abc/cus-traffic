a2f1ec35a27b26dcc23cd942ec5c3d1bd83dbd6b
不染纤尘醉一笑 <17615186105@163.com>
2026-02-03 14:48:55 +0800

    +a

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,6 @@
 import { youshe, ali55, pangmen } from "@/fonts/index";
 import HeaderRC from "./components/header";
-// import MapContainer from "./ui/map/index";
+import MapContainer from "./ui/map/index";
 
 const layoutBg = "#02171C";
 //   "linear-gradient(180deg, rgba(16, 21, 32, 0.00) 0%, rgba(18, 21, 29, 0.77) 24.87%, #12151E 45.86%, rgba(17, 20, 29, 0.79) 74.71%, rgba(16, 21, 32, 0.00) 92.62%);url";
@@ -15,7 +15,7 @@ function DashboardLayout({ children }) {
        ${youshe.variable} ${ali55.variable} ${pangmen.variable}
       `}
     >
-      {/* <MapContainer></MapContainer> */}
+      <MapContainer></MapContainer>
 
       {/* 中间加一层透光的 */}
       <div
